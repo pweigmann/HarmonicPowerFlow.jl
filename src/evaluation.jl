35f711f6403293c96a93a31c8a3bca2f09dd04f0
Pascal Weigmann <p.weigmann@posteo.de>
2021-07-15 17:52:35 +0200

    added function to check limits

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -1,3 +1,5 @@
+using DataFrames
+
 """
     THD(u)
 
@@ -17,6 +19,36 @@ function THD(u)
         THD[ID, "THD_R"] = sqrt(sum([u[h].v[ID]^2 for h in harmonics[2:end]]))./sqrt(sum([u[h].v[ID]^2 for h in harmonics]))
     end
     return THD
+end
+
+
+"""
+    EN50160(u, nodeID)
+
+Check if harmonic limits according to EN 50160 and EN 61000 are met at node."""
+function limits(u, nodeID)
+    h = [3, 5, 7, 9, 11, 13, 15]
+    v = [u[i].v[nodeID] for i in h]
+    limit_EN50160 = [0.05, 0.06, 0.05, 0.015, 0.035, 0.03, 0.005]
+    limit_EN61000 = [0.05, 0.06, 0.05, 0.015, 0.035, 0.03, 0.005]
+    met_EN50160 = collect(v .< limit_EN50160)
+    met_EN61000 = collect(v .< limit_EN61000)
+    
+    if all(met_EN50160) && all(met_EN6100)
+        println("Harmonic limits met at node "*string(nodeID))
+    else
+        println("Harmonic limits not met at node "*string(nodeID)*"! Check DataFrame for details.")
+    end
+
+    THD = HarmonicPowerFlow.THD(u).THD_F[nodeID]
+
+    if THD < 0.08
+        println("THD at node "*string(nodeID)*" within limits (< 8%).")
+    else
+        println("THD at node "*string(nodeID)*" exceeds limits (> 8%)!")
+    end
+
+    DataFrame(v=v, within_EN50160 = met_EN50160, within_EN61000=met_EN61000)
 end
 
 
